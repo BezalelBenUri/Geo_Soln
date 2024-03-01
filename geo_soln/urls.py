32bde7d4740896225437c1a8c3bef39e528f895c
BezalelBenUri <diamondinyang@.com>
2024-03-01 14:07:09 +0100

    integrating drf-spectacular for documentation

--- geo_soln/urls.py
+++ geo_soln/urls.py
@@ -18,7 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("soln.urls"), name = "healthfaciliites"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name = "schema"),
+    path("api/v1/schema/redoc/", SpectacularRedocView.as_view(), name = "redoc"),
+    path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(), name = "swagger-ui"),
+
 ]
